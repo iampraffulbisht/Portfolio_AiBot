b5735ec78c90ccc3ba332b06be1561cc8e136a4b
Prafful Bisht <114369813+iampraffulbisht@users.noreply.github.com>
2024-07-07 04:01:17 +0530

    Update portfolio.py

--- portfolio.py
+++ portfolio.py
@@ -50,7 +50,7 @@ with col1:
     st.subheader("Hi :wave:")
     st.markdown("<h1>I am Prafful<br>Bisht</h1>", unsafe_allow_html=True)
 with col2:
-    st.image("images/Praffulbisht.png",width=600)
+    st.image("images/praffulbisht.png",width=600)
 
 # About me Section
 st.write("")
